722ad25240c5aa740969182a73217e333f6bd634
laur <laur.aliste@gmail.com>
2024-10-28 13:49:53 +0100

    unrar: change build process to use docker buildkit
    - avoids the unnecessary step of actually tagging an image & starting a
      container just to copy the built assets

--- deps/unrar/build-unrar.sh
+++ deps/unrar/build-unrar.sh
@@ -3,20 +3,12 @@
 # builds unrar pkg off Dockerfile, and copies results to $TARGET_DIR
 
 #####################################
-readonly SELF="${0##*/}"
 DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"  # location of this script
-TAG="${SELF}-$RANDOM"
 TARGET_DIR="${DIR}/../builds"
 #####################################
 
 [[ -d "$TARGET_DIR" ]] || exit 1
-
-# build image that buids unrar pkg:
-docker build -t "$TAG" -- . || exit 1
-# start the container so we can copy packages to the mount (docker build doesn't allow mounts, hence why we do this):
-docker run --rm -v "${TARGET_DIR}:/out" -- "$TAG" find /build/unrar-target -maxdepth 1 -mindepth 1 -type f -exec cp -t /out {} + || exit 1
-
-docker image rm "$TAG" || exit 1
+docker build  --output "$TARGET_DIR" .
 
 exit 0
 
